d70e0bda02a7b7c8d59ee27d80211b0cd653e6d4
Ar Kar <arkar20011@gmail.com>
2021-05-31 14:34:43 +0630

    learning Redux part-3 composition array

--- Redux/index.jsx
+++ Redux/index.jsx
@@ -1,18 +1,32 @@
-const todos=(state=[],action)=>{
-	switch(action.type){
-		case("ADD_TODO"):
-	return [
-		{id:1,
-		body:"SOme LOrem",
-		completed:false}
-	]
+const todo = (state, action) => {
+	switch (action.type) {
+		case ("ADD_TODO"):
+			return [
+				{
+					id: 1,
+					body: "SOme LOrem",
+					completed: false
+				}
+				
+			];
 		case ("TOGGLE_TODO"):
-			return state.map(state => {
-				if (state.id !== action.id) {
+			if (state.id !== action.id) {
 					return state;
 				}
 			return { ...state, completed: !state.completed }
 
+	}
+}
+
+
+const todos = (state = [], action) => {
+	switch(action.type){
+		case ("ADD_TODO"):
+			todo(undefined,action)
+	
+		case ("TOGGLE_TODO"):
+			return state.map(state => {
+				todo(stae,action)
 			})
 		
 		default:
