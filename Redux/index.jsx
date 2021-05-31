3ab9f3f0a8784f080b9e309130e19abfc444f6d9
Ar Kar <arkar20011@gmail.com>
2021-05-31 16:19:14 +0630

    learning Redux part-3 composition object && combine reducer

--- Redux/index.jsx
+++ Redux/index.jsx
@@ -1,89 +1,126 @@
+/*
+ * Open the console
+ * to see the state log.
+ */
+
 const todo = (state, action) => {
-	switch (action.type) {
-		case ("ADD_TODO"):
-			return [
-				{
-					id: 1,
-					body: "SOme LOrem",
-					completed: false
-				}
-				
-			];
-		case ("TOGGLE_TODO"):
-			if (state.id !== action.id) {
-					return state;
-				}
-			return { ...state, completed: !state.completed }
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
 
-	}
-}
-
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+};
 
 const todos = (state = [], action) => {
-	switch(action.type){
-		case ("ADD_TODO"):
-			todo(undefined,action)
-	
-		case ("TOGGLE_TODO"):
-			return state.map(state => {
-				todo(stae,action)
-			})
-		
-		default:
-			return state
-	}
-}
-const testtoggletodos = () => {
-	const stateBefore = [
-		{
-			id: 1,
-			title: 'Lorem',
-			completed: false
-		},
-		{
-			id: 2,
-			title: 'Lorem',
-			completed: false
-		}
-	];
-	const stateAfter = [
-		{
-			id: 1,
-			title: 'Lorem',
-			completed: true
-		},
-		{
-			id: 2,
-			title: 'Lorem',
-			completed: false
-		}
-	];
-	const action = {
-		type: 'TOGGLE_TODO',
-		id:1
-	}
-
-	expect(todos(stateBefore,action)).toEqual(stateAfter)
-	
-}
-
-const testTodo=()=>{
-	const stateBefore=[]
-	const action={
-		type:"ADD_TODO",
-		id:1,
-		body:"SOme LOrem"
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
 };
-	const stateAfter=[
-	{	id:1,
-		body:"SOme LOrem",
-		completed:false}
-	]
 
-expect(todos(stateBefore,action)).toEqual(stateAfter)
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
 
+const { combineReducers } = Redux;
+const todoApp = combineReducers({
+	todos: todos,
+	visibilityFilter:visibilityFilter
+})
+
+// const todoApp = (state = {}, action) => {
+//   return {
+//     todos: todos(
+//       state.todos,
+//       action
+//     ),
+//     visibilityFilter: visibilityFilter(
+//       state.visibilityFilter,
+//       action
+//     )
+//   };
+// };
+
+const { createStore } = Redux;
+const store = createStore(todoApp);
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.')
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+
+
+// display in plunker preview
+function displayInPreview(string) {
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(string);
+  newDiv.appendChild(newContent);
+  document.body.appendChild(newDiv)
 }
-
-testTodo()
-testtoggletodos()
-console.log("All TEst Are Passing")
